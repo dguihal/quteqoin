ba66554c3e240923b7e270dcb5ad91b930ac8a22
dguihal@gmail.com <dguihal@gmail.com@33cb4d4e-50df-bee8-dd5f-3bf00d3a7b00>
2012-03-15 22:28:49 +0000

    Suppression de la mention @bouchot d'une norloge insérée lorsque c'est inutile

--- core/qqnorloge.h
+++ core/qqnorloge.h
@@ -13,13 +13,30 @@ public:
     QString toStringPalmi();
     QString toStringPini();
 
-    static QRegExp norlogeRegexp() {return QRegExp(QString::fromAscii("((?:(?:1[0-2]|0[1-9])/(?:3[0-1]|[1-2][0-9]|0[1-9])#)?" //date
-                                                                            "(?:2[0-3]|[0-1][0-9]):(?:[0-5][0-9])(?::[0-5][0-9])?" //time
-                                                                            "(?:[¹²³]|[:\\^][1-9]|[:\\^][1-9][0-9])?" //subtime
-                                                                            "(?:@[A-Za-z0-9_]+)?)"), //tribune
-                                                         Qt::CaseSensitive,
-                                                         QRegExp::RegExp); }
+    //Les zones capturées sont :
+    // 1 - Date + Time + Subtime
+    // 2 - Date
+    // 3 - Time + Subtime
+    // 4 - Tribune
+    static QRegExp norlogeRegexp()
+    {
+        return norlogeRegexp(QString::fromAscii("[A-Za-z0-9_]+"));
+    }
 
+    //Les zones capturées sont :
+    // 1 - Date + Time + Subtime
+    // 2 - Date
+    // 3 - Time + Subtime
+    // 4 - Tribune
+    static QRegExp norlogeRegexp(const QString & bouchot)
+    {
+        return QRegExp(QString::fromAscii("(((?:1[0-2]|0[1-9])/(?:3[0-1]|[1-2][0-9]|0[1-9])#)?" //date
+                                          "((?:2[0-3]|[0-1][0-9]):(?:[0-5][0-9])(?::[0-5][0-9])?" //time
+                                          "(?:[¹²³]|[:\\^][1-9]|[:\\^][1-9][0-9])?))" //subtime
+                                          "(@") + bouchot + QString::fromAscii(")?"), //tribune
+                       Qt::CaseSensitive,
+                       QRegExp::RegExp);
+    }
 private:
     QString m_bouchot;
     QDateTime m_date;
