dd26a1b13df8f2034baa2d60ca77029b150b672a
dguihal@gmail.com <dguihal@gmail.com@33cb4d4e-50df-bee8-dd5f-3bf00d3a7b00>
2012-04-19 20:48:00 +0000

    Amélioration de la fenêtre de dialogue sur la demande d'authentification sur le proxy

--- trunk/ui/qqproxyauthdialog.cpp
+++ trunk/ui/qqproxyauthdialog.cpp
@@ -1,11 +1,15 @@
 #include "qqproxyauthdialog.h"
 #include "ui_qqproxyauthdialog.h"
 
+#include <QApplication>
+
 QQProxyAuthDialog::QQProxyAuthDialog(QWidget *parent) :
 	QDialog(parent),
 	ui(new Ui::QQProxyAuthDialog)
 {
 	ui->setupUi(this);
+	ui->iconLabel->setText(QString());
+	ui->iconLabel->setPixmap(QApplication::style()->standardIcon(QStyle::SP_MessageBoxQuestion, 0, parent).pixmap(32, 32));
 	setModal(true);
 }
 
@@ -16,21 +20,21 @@ QQProxyAuthDialog::~QQProxyAuthDialog()
 
 void QQProxyAuthDialog::setLogin(QString & login)
 {
-	return ui->loginLineEdit->setText(login);
+	return ui->userNameLineEdit->setText(login);
 
 }
 
 QString QQProxyAuthDialog::login()
 {
-	return ui->loginLineEdit->text();
+	return ui->userNameLineEdit->text();
 }
 
 void QQProxyAuthDialog::setPasswd(QString & passwd)
 {
-	return ui->passwdLineEdit->setText(passwd);
+	return ui->passwordLineEdit->setText(passwd);
 }
 
 QString QQProxyAuthDialog::passwd()
 {
-	return ui->passwdLineEdit->text();
+	return ui->passwordLineEdit->text();
 }
