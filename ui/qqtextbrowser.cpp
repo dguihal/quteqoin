80280aa9d390dc7bf00ca5dec0b7b8f586444ccf
dguihal@gmail.com <dguihal@gmail.com@33cb4d4e-50df-bee8-dd5f-3bf00d3a7b00>
2012-07-29 19:34:54 +0000

    Modification interne, plutôt que de dupliquer les données du Post dans le bloc message on stocke un pointeur vers le post.

--- ui/qqtextbrowser.cpp
+++ ui/qqtextbrowser.cpp
@@ -1,5 +1,6 @@
 #include "qqtextbrowser.h"
 
+#include "core/qqbouchot.h"
 #include "core/qqpost.h"
 #include "ui/qqmessageblockuserdata.h"
 
@@ -10,14 +11,14 @@
 #include <QTextTableCell>
 
 QQTextBrowser::QQTextBrowser(QString groupName, QQPinipede *parent) :
-    QTextEdit(parent)
+	QTextEdit(parent)
 {
-    setFrameStyle(QFrame::NoFrame);
-    setReadOnly(true);
-    m_groupName = groupName;
-    m_parent = parent;
-    m_highlightAsked = false;
-    this->setMouseTracking(true);
+	setFrameStyle(QFrame::NoFrame);
+	setReadOnly(true);
+	m_groupName = groupName;
+	m_parent = parent;
+	m_highlightAsked = false;
+	this->setMouseTracking(true);
 }
 
 QQTextBrowser::~QQTextBrowser()
@@ -26,100 +27,101 @@ QQTextBrowser::~QQTextBrowser()
 
 void QQTextBrowser::mouseMoveEvent(QMouseEvent *event)
 {
-    qDebug() << "####################################";
-    qDebug() << "QQTextBrowser::mouseMoveEvent x=" << event->x() << ",y=" << event->y();
-    qDebug() << "####################################";
-    QTextEdit::mouseMoveEvent(event);
+	qDebug() << "####################################";
+	qDebug() << "QQTextBrowser::mouseMoveEvent x=" << event->x() << ",y=" << event->y();
+	qDebug() << "####################################";
+	QTextEdit::mouseMoveEvent(event);
 
-    bool highlightActivated = false;
+	bool highlightActivated = false;
 
-    mousePressed = false;
+	mousePressed = false;
 
-    QTextCursor cursor = cursorForPosition(event->pos());
+	QTextCursor cursor = cursorForPosition(event->pos());
 
-    QTextBlock block = cursor.block();
-    QQMessageBlockUserData * blockData = dynamic_cast<QQMessageBlockUserData *>(block.userData());
+	QTextBlock block = cursor.block();
+	QQMessageBlockUserData * blockData = dynamic_cast<QQMessageBlockUserData *>(block.userData());
 
-    if(blockData != NULL)
-    {
-        //Zone message
-        QQNorlogeRef nRef;
+	if(blockData != NULL)
+	{
+		//Zone message
+		QQNorlogeRef nRef;
 
-        if(blockData->constainsData(QQMessageBlockUserData::IS_MESSAGE_ZONE))
-        {
-            //Est-on au dessus d'une norloge
-            nRef = blockData->norlogeRefForIndex(cursor.positionInBlock());
-        }
-        else if(blockData->constainsData(QQMessageBlockUserData::IS_NORLOGE_ZONE))
-        {
-            QString norlogeString = blockData->getData(QQMessageBlockUserData::POST_NORLOGE).toString();
-            nRef = QQNorlogeRef(blockData->getData(QQMessageBlockUserData::BOUCHOT_NAME).toString(),
-                                norlogeString, norlogeString, 1);
+		if(blockData->blockZone() == QQMessageBlockUserData::MESSAGE_ZONE)
+		{
+			//Est-on au dessus d'une norloge
+			nRef = blockData->norlogeRefForIndex(cursor.positionInBlock());
+		}
+		else if(blockData->blockZone() == QQMessageBlockUserData::NORLOGE_ZONE)
+		{
+			QPointer<QQPost> post = blockData->post();
+			Q_ASSERT(!post.isNull());
+			QString norlogeString = post.data()->norloge();
+			nRef = QQNorlogeRef(post.data()->bouchot()->name(),
+								norlogeString, norlogeString, 1);
 
-        }
-        qDebug() << "QQTextBrowser::mouseMoveEvent, str = " << nRef.getOrigNRef() << " position : " << nRef.getPosInMessage();
-        if(nRef.getOrigNRef().length() > 0)
-        {
-            if(m_highlightedBlockUserData != blockData || m_highlightedNRef != nRef)
-            {
-                m_highlightedBlockUserData = blockData;
-                m_highlightedNRef = nRef;
-                qDebug() << "QQTextBrowser::mouseMoveEvent norlogeRefHovered";
-                emit unHighlight();
-                emit norlogeRefHovered(nRef);
-            }
-            highlightActivated = true;
-        }
-    }
+		}
+		qDebug() << "QQTextBrowser::mouseMoveEvent, str = " << nRef.getOrigNRef() << " position : " << nRef.getPosInMessage();
+		if(nRef.getOrigNRef().length() > 0)
+		{
+			if(m_highlightedBlockUserData != blockData || m_highlightedNRef != nRef)
+			{
+				m_highlightedBlockUserData = blockData;
+				m_highlightedNRef = nRef;
+				qDebug() << "QQTextBrowser::mouseMoveEvent norlogeRefHovered";
+				emit unHighlight();
+				emit norlogeRefHovered(nRef);
+			}
+			highlightActivated = true;
+		}
+	}
 
-    qDebug() << "highlightActivated = " << highlightActivated;
-    if(m_highlightedBlockUserData != NULL && highlightActivated == false)
-    {
-        m_highlightedBlockUserData = NULL;
-        m_highlightedNRef = QQNorlogeRef();
-        emit unHighlight();
-    }
+	qDebug() << "highlightActivated = " << highlightActivated;
+	if(m_highlightedBlockUserData != NULL && highlightActivated == false)
+	{
+		m_highlightedBlockUserData = NULL;
+		m_highlightedNRef = QQNorlogeRef();
+		emit unHighlight();
+	}
 }
 
 void QQTextBrowser::mousePressEvent ( QMouseEvent * event )
 {
-    QTextEdit::mousePressEvent(event);
+	QTextEdit::mousePressEvent(event);
 
-    mousePressed = true;
+	mousePressed = true;
 }
 
 void QQTextBrowser::mouseReleaseEvent(QMouseEvent * event)
 {
-    QTextEdit::mouseReleaseEvent(event);
+	QTextEdit::mouseReleaseEvent(event);
 
-    //pour ne pas confondre clic et selection
-    if(mousePressed == false)
-        return;
-    mousePressed = false;
+	//pour ne pas confondre clic et selection
+	if(mousePressed == false)
+		return;
+	mousePressed = false;
 
-    QTextCursor cursor = cursorForPosition(event->pos());
-    QTextBlock block = cursor.block();
-    QQMessageBlockUserData * blockData = dynamic_cast<QQMessageBlockUserData *>(block.userData());
+	QTextCursor cursor = cursorForPosition(event->pos());
+	QTextBlock block = cursor.block();
+	QQMessageBlockUserData * blockData = dynamic_cast<QQMessageBlockUserData *>(block.userData());
 
-    if(blockData != NULL)
-    {
-        if(blockData->constainsData(QQMessageBlockUserData::IS_NORLOGE_ZONE) &&
-                blockData->getData(QQMessageBlockUserData::IS_NORLOGE_ZONE) == true)
-        {
-            QString bouchot = blockData->getData(QQMessageBlockUserData::BOUCHOT_NAME).toString();
-            QString postNorloge = blockData->getData(QQMessageBlockUserData::POST_NORLOGE).toString();
+	if(blockData != NULL)
+	{
+		if(blockData->blockZone() == QQMessageBlockUserData::NORLOGE_ZONE)
+		{
+			QPointer<QQPost> ptrPost = blockData->post();
+			Q_ASSERT(!ptrPost.isNull());
 
-            QQNorloge norloge(bouchot, postNorloge);
-            emit norlogeClicked(norloge);
-        }
-        else if(blockData->constainsData(QQMessageBlockUserData::IS_LOGIN_UA_ZONE) &&
-                blockData->getData(QQMessageBlockUserData::IS_LOGIN_UA_ZONE) == true)
-            emit loginClicked(m_groupName);
-    }
+			QQNorloge norloge(ptrPost.data()->bouchot()->name(),
+							  ptrPost.data()->norloge());
+			emit norlogeClicked(norloge);
+		}
+		else if(blockData->blockZone() == QQMessageBlockUserData::LOGIN_UA_ZONE)
+			emit loginClicked(m_groupName);
+	}
 }
 
 void QQTextBrowser::paintEvent( QPaintEvent * event )
 {
-    QTextEdit::paintEvent(event);
+	QTextEdit::paintEvent(event);
 
 }
