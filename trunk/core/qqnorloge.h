8cc731bca1565cee8fef32e75115d4eebdf3f332
dguihal@gmail.com <dguihal@gmail.com@33cb4d4e-50df-bee8-dd5f-3bf00d3a7b00>
2013-02-12 09:42:06 +0000

    Bon un petit correctif pour le backhighlight en espérant que ce coup ci sera le bon ...

--- trunk/core/qqnorloge.h
+++ trunk/core/qqnorloge.h
@@ -7,28 +7,28 @@
 class QQNorloge
 {
 public:
-    QQNorloge();
-    QQNorloge(QString bouchot, QString dateh);
-    QQNorloge(const QQNorloge& norloge);
+	QQNorloge();
+	QQNorloge(QString bouchot, QString dateh);
+	QQNorloge(const QQNorloge& norloge);
 
-    QString toStringPalmi();
-    QString toStringPini();
-    QString srcBouchot() { return m_srcBouchot; }
+	QString toStringPalmi();
+	QString toStringPini();
+	QString srcBouchot() { return m_srcBouchot; }
 
-    void setNorlogeIndex(int index) { m_norlogeIndex = index;}
-    int norlogeIndex() { return m_norlogeIndex; }
+	void setNorlogeIndex(int index) { m_norlogeIndex = index;}
+	int norlogeIndex() { return m_norlogeIndex; }
 
 protected:
-    QString m_srcBouchot;
+	QString m_srcBouchot;
 
-    QString m_dateYearPart;
-    QString m_dateMonthPart;
-    QString m_dateDayPart;
-    QString m_dateHourPart;
-    QString m_dateMinutePart;
-    QString m_dateSecondPart;
+	QString m_dateYearPart;
+	QString m_dateMonthPart;
+	QString m_dateDayPart;
+	QString m_dateHourPart;
+	QString m_dateMinutePart;
+	QString m_dateSecondPart;
 
-    int m_norlogeIndex;
+	int m_norlogeIndex;
 };
 
 #endif // QQNORLOGE_H
