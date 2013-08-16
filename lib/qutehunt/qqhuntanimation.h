06acf283309b7b923462ad551d537e960c85a019
dguihal@gmail.com <dguihal@gmail.com@33cb4d4e-50df-bee8-dd5f-3bf00d3a7b00>
2013-08-16 22:41:47 +0000

    Virage des protobufs et nanims (j'y comprends rien), place aux QAbstractAnimation (a venir)

---
+++ lib/qutehunt/qqhuntanimation.h
@@ -0,0 +1,17 @@
+#ifndef QQHUNTANIMATION_H
+#define QQHUNTANIMATION_H
+
+#include "qutehunt_global.h"
+
+#include <QStringList>
+
+class QUTEHUNTSHARED_EXPORT QQHuntAnimation
+{
+
+public:
+	QQHuntAnimation();
+
+	static QStringList huntAnimations();
+};
+
+#endif // QQHUNTANIMATION_H
