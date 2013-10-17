f384fefd87b05cc1d0932d1b5257e2ba03ad97c7
dguihal@gmail.com <dguihal@gmail.com@33cb4d4e-50df-bee8-dd5f-3bf00d3a7b00>
2013-10-17 15:22:21 +0000

    Refactoring pour pouvoir ajouter facilement le tronçonnage de table

---
+++ trunk/core/qqhuntpixmapitem.h
@@ -0,0 +1,69 @@
+#ifndef QQHUNTPIXMAPITEM_H
+#define QQHUNTPIXMAPITEM_H
+
+#include <QGenericMatrix>
+#include <QGraphicsPixmapItem>
+#include <QSequentialAnimationGroup>
+#include <QTimer>
+#include <QVector>
+
+typedef QGenericMatrix<1, 2, float> QQMatrix1x2;
+#define X_VALUE 0,0
+#define Y_VALUE 1,0
+
+typedef QGenericMatrix<2, 2, float> QQMatrix2x2;
+
+class QQHuntPixmapItem : public QObject, public QGraphicsPixmapItem
+{
+	Q_OBJECT
+	Q_PROPERTY(QPointF pos READ pos WRITE setPos)
+public:
+	explicit QQHuntPixmapItem(QString srcBouchot, QString postId, QObject *parent = 0);
+	~QQHuntPixmapItem();
+
+
+	QString bouchotName() const { return m_srcBouchot; }
+	QString postId() const { return m_postId; }
+
+signals:
+
+public slots:
+	void animate();
+	void animateKill();
+	void animateMove();
+	void loadNextPixMap();
+
+protected:
+
+	class QQPixmapProp
+	{
+	public:
+		explicit QQPixmapProp() : m_pixmapName() { m_rotationAngle = 0.0, m_mirrorX = false, m_mirrorY = false; }
+		QString m_pixmapName;
+		float m_rotationAngle;
+		bool m_mirrorX;
+		bool m_mirrorY;
+	};
+
+	virtual QQPixmapProp animPixmapProp(float angle) = 0;
+	virtual QQPixmapProp fallPixmapProp() = 0;
+	virtual QQPixmapProp killPixmapProp() = 0;
+	void nextMoveStep();
+
+	QSequentialAnimationGroup *m_animation;
+	QTimer m_animPixmapTimer;
+	bool m_animPixmapTimerEnabled;
+	QVector<QQPixmapProp> m_listPixmapProp;
+	int m_maxAnimPixmapNumInc;
+	int m_minAnimPixmapNumInc;
+	int m_animPixmapNum;
+	bool m_animPixmapNumInc;
+	QString m_currPixmapName;
+
+	QQMatrix1x2 m_speedVec;
+
+	QString m_srcBouchot;
+	QString m_postId;
+};
+
+#endif // QQHUNTPIXMAPITEM_H
--- trunk/ui/qqduckpixmapitem.h
+++ trunk/ui/qqduckpixmapitem.h
@@ -1,60 +1,20 @@
 #ifndef QQDUCKPIXMAPITEM_H
 #define QQDUCKPIXMAPITEM_H
 
-#include <QGenericMatrix>
-#include <QGraphicsPixmapItem>
-#include <QSequentialAnimationGroup>
-#include <QTimer>
-#include <QVector>
+#include "core/qqhuntpixmapitem.h"
 
-typedef QGenericMatrix<1, 2, float> QQMatrix1x2;
-typedef QGenericMatrix<2, 2, float> QQMatrix2x2;
-
-class QQDuckPixmapItem : public QObject, public QGraphicsPixmapItem
+class QQDuckPixmapItem : public QQHuntPixmapItem
 {
 	Q_OBJECT
 	Q_PROPERTY(QPointF pos READ pos WRITE setPos)
 public:
 	explicit QQDuckPixmapItem(QString srcBouchot, QString postId, QObject *parent = 0);
-	~QQDuckPixmapItem();
+	~QQDuckPixmapItem() {}
 
-
-	QString bouchotName() const { return m_srcBouchot; }
-	QString postId() const { return m_postId; }
-
-signals:
-
-public slots:
-	void animate();
-	void animateKill();
-	void animateMove();
-	void loadNextPixMap();
-
-private:
-	void nextMoveStep();
-
-	QSequentialAnimationGroup *m_animation;
-
-	QQMatrix1x2 m_speedVec;
-
-	class QQPixmapProp
-	{
-	public:
-		explicit QQPixmapProp() : m_pixmapName() { m_rotationAngle = 0.0, m_mirrorX = false, m_mirrorY = false; }
-		QString m_pixmapName;
-		double m_rotationAngle;
-		bool m_mirrorX;
-		bool m_mirrorY;
-	};
-	QString m_srcBouchot;
-	QString m_postId;
-
-	QVector<QQPixmapProp> m_listPixmapProp;
-	int m_animPixmapNum;
-	bool m_animPixmapNumInc;
-	bool m_animPixmapTimerEnabled;
-	QTimer m_animPixmapTimer;
-	QString m_currPixmapName;
+protected:
+	virtual QQHuntPixmapItem::QQPixmapProp animPixmapProp(float angle);
+	virtual QQHuntPixmapItem::QQPixmapProp fallPixmapProp();
+	virtual QQHuntPixmapItem::QQPixmapProp killPixmapProp();
 };
 
 #endif // QQDUCKPIXMAPITEM_H
