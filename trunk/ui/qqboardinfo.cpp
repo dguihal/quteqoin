917e160fb27d8be756f7fcb78cfd731cad36b859
dguihal@gmail.com <dguihal@gmail.com@33cb4d4e-50df-bee8-dd5f-3bf00d3a7b00>
2013-11-06 10:08:57 +0000

    Mise en avant de problème de refresh de backend, un peu de reorg de code

--- trunk/ui/qqboardinfo.cpp
+++ trunk/ui/qqboardinfo.cpp
@@ -5,10 +5,16 @@
 
 #include <QtDebug>
 
+#define QPROGRESSBAR_COLOR_OK_SS "QProgressBar { border: none; background-color: rgba(0, 0, 0, 0%);}"
+#define QPROGRESSBAR_COLOR_KO_SS "QProgressBar { border: none; background-color: rgba(0, 0, 0, 0%); color: red}"
+#define QPROGRESSBAR_CHUNK_SS "QProgressBar::chunk {background-color: %1; width: 5px; margin: 0.5px;}"
+
+
 QQBoardInfo::QQBoardInfo(QQBouchot *board, QWidget *parent) :
 	QWidget(parent),
 	m_ui(new Ui::QQBoardInfo),
-	m_board(board)
+	m_board(board),
+	m_refreshFailed(false)
 {
 	if(m_board == NULL)
 		return;
@@ -23,10 +29,10 @@ QQBoardInfo::QQBoardInfo(QQBouchot *board, QWidget *parent) :
 	m_ui->refreshPB->setTextVisible(true);
 	m_ui->refreshPB->setAlignment(Qt::AlignLeft | Qt::AlignVCenter);
 	m_ui->refreshPB->setFormat(m_board->name());
-	m_ui->refreshPB->setStyleSheet(QString("QProgressBar { border: none; background-color: rgba(0, 0, 0, 0%)} ")
-								   .append("QProgressBar::chunk {background-color: ")
-								   .append(m_board->settings().color().name())
-								   .append("; width: 5px; margin: 0.5px;}"));
+	m_ui->refreshPB->setStyleSheet(QString(QPROGRESSBAR_COLOR_OK_SS)
+								   .append(" ")
+								   .append(QPROGRESSBAR_CHUNK_SS)
+								   .arg(m_board->settings().color().name()));
 
 	m_pctPollAnimation.setStartValue(0);
 	m_pctPollAnimation.setEndValue(100);
@@ -35,6 +41,7 @@ QQBoardInfo::QQBoardInfo(QQBouchot *board, QWidget *parent) :
 	m_pctPollAnimation.setPropertyName("value");
 	rearmRefreshPB();
 	connect(m_board, SIGNAL(lastPostersUpdated()), this, SLOT(updateUserList()));
+	connect(m_board, SIGNAL(refreshError()), this, SLOT(showRefreshError()));
 	connect(m_board, SIGNAL(refreshStarted()), this, SLOT(rearmRefreshPB()));
 }
 
@@ -46,9 +53,26 @@ QQBoardInfo::~QQBoardInfo()
 void QQBoardInfo::rearmRefreshPB()
 {
 	m_pctPollAnimation.stop();
+	if(m_refreshFailed)
+	{
+		m_ui->refreshPB->setStyleSheet(QString(QPROGRESSBAR_COLOR_OK_SS)
+									   .append(" ")
+									   .append(QPROGRESSBAR_CHUNK_SS)
+									   .arg(m_board->settings().color().name()));
+		m_refreshFailed = false;
+	}
 	m_ui->refreshPB->setValue(0);
 	m_pctPollAnimation.setDuration(m_board->settings().refresh() * 1000);
 	m_pctPollAnimation.start();
+}
+
+void QQBoardInfo::showRefreshError()
+{
+	m_ui->refreshPB->setStyleSheet(QString(QPROGRESSBAR_COLOR_KO_SS)
+								   .append(" ")
+								   .append(QPROGRESSBAR_CHUNK_SS)
+								   .arg(m_board->settings().color().name()));
+	m_refreshFailed = true;
 }
 
 void QQBoardInfo::toggleExpandedView()
