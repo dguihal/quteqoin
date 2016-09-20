cbbe70f1d8f23b88e7b837b8428936914a8c54f9
Damien Guihal <dguihal@gmail.com>
2016-09-20 17:00:23 +0200

    Remove outdated pastelink support (doesn't work anymore service has changed)

--- ui/palmipede/qqpalmifileposter.cpp
+++ ui/palmipede/qqpalmifileposter.cpp
@@ -42,9 +42,6 @@ bool QQPalmiFilePoster::postFile(const QString &fileName)
 
 	switch (m_service)
 	{
-	case PASTELINK:
-		postFilePasteLink(file);
-		break;
 	case UPLOAD_3TER_ORG:
 		postFileUpload3TerOrg(file);
 		break;
@@ -70,9 +67,6 @@ void QQPalmiFilePoster::requestFinishedSlot(QNetworkReply *reply)
 		QString s = QString(reply->readAll());
 		switch (m_service)
 		{
-		case PASTELINK:
-			parsePasteLinkResponse(s);
-			break;
 		case UPLOAD_3TER_ORG:
 			parseUpload3TerOrg(s);
 			break;
@@ -87,61 +81,6 @@ void QQPalmiFilePoster::requestFinishedSlot(QNetworkReply *reply)
 		emit postErr(reply->errorString());
 
 	reply->deleteLater();
-}
-
-//////////////
-/// \brief QQPalmiFilePoster::postFilePasteLink
-/// \param file
-///
-void QQPalmiFilePoster::postFilePasteLink(QFile *file)
-{
-	QHttpMultiPart *multiPart = new QHttpMultiPart(QHttpMultiPart::FormDataType);
-
-	QHttpPart filePart;
-#if(QT_VERSION >= QT_VERSION_CHECK(5, 0, 0))
-	QMimeDatabase mimeDatabase;
-	QMimeType mimeType = mimeDatabase.mimeTypeForFile(file->fileName(), QMimeDatabase::MatchDefault);
-	filePart.setHeader(QNetworkRequest::ContentTypeHeader, QVariant(mimeType.name()));
-#else
-	filePart.setHeader(QNetworkRequest::ContentTypeHeader, QVariant("application/octet-stream"));
-#endif
-	filePart.setHeader(QNetworkRequest::ContentDispositionHeader,
-					   QString("form-data; name=\"files[]\"; filename=\"%1\"").arg(file->fileName()));
-	filePart.setBodyDevice(file);
-	file->setParent(multiPart); // we cannot delete the file now, so delete it with the multiPart
-
-	multiPart->append(filePart);
-
-	QUrl url("http://pastelink.me/server/php/");
-	QNetworkRequest request(url);
-	request.setRawHeader("User-Agent", "Mozilla/5.0 (quteqoin)"); // Le service n'accepte pas l'ua par défaut ...
-	request.setRawHeader("Accept", "application/json, text/javascript, */*; q=0.01");
-	request.setRawHeader("Accept-Language", "en-US,en;q=0.5");
-	//request.setRawHeader("Accept-Encoding", "gzip, deflate"); // Qt 4 ne supporte pas gunzip nativement ....
-	request.setRawHeader("X-Requested-With", "XMLHttpRequest");
-	request.setRawHeader("Referer", "http://pastelink.me/");
-
-	QNetworkReply *reply = httpPost(request, multiPart);
-	multiPart->setParent(reply); // delete the multiPart with the reply
-}
-
-void QQPalmiFilePoster::parsePasteLinkResponse(const QString &data)
-{
-	//JSON seulement supporté par Qt 5
-#if(QT_VERSION >= QT_VERSION_CHECK(5, 0, 0))
-		QJsonDocument d = QJsonDocument::fromBinaryData(data.toLatin1());
-		QJsonObject o = d.array().at(0).toObject();
-		emit finished(QString("http://pastelink.me/dl/") + o["key"].toString());
-#else
-		int i = data.indexOf("\"key\":\"");
-		if(i >= 0)
-		{
-			int j = data.indexOf("\"", i);
-			int start = i + 7; // len /"key":"/
-			QString key = data.mid(start, j - start);
-			emit finished(QString("http://pastelink.me/dl/") + key);
-		}
-#endif
 }
 
 ////////////
--- ui/palmipede/qqpalmifileposter.h
+++ ui/palmipede/qqpalmifileposter.h
@@ -9,8 +9,7 @@ class QQPalmiFilePoster : public QQNetworkAccessor
 {
 	Q_OBJECT
 public:
-	//enum Services { PASTELINK, CJOINT };
-	enum Services { PASTELINK, UPLOAD_3TER_ORG, JUS_Y_FR };
+	enum Services { UPLOAD_3TER_ORG, JUS_Y_FR };
 	explicit QQPalmiFilePoster(QObject *parent = 0);
 
 	bool postFile(const QString &fileName);
@@ -25,8 +24,6 @@ protected slots:
 	virtual void requestFinishedSlot(QNetworkReply *reply);
 
 private:
-	void postFilePasteLink(QFile *file);
-	void parsePasteLinkResponse(const QString &data);
 	void postFileUpload3TerOrg(QFile *file);
 	void parseUpload3TerOrg(const QString &s);
 	void postFileJusYFr(QFile *file);
