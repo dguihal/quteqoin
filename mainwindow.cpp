ea8710f023c43ece46d6dcddb83a109ebdc599b3
Damien Guihal <dguihal@gmail.com>
2017-06-01 00:34:01 +0200

    Suppression d un commentaire qui n aurait jamais du etre commite ... Corrige #23

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -336,7 +336,7 @@ void MainWindow::bouchotDestroyed(QQBouchot *bouchot)
 	QString name = bouchot->name();
 	QString group = bouchot->settings().group();
 
-	//	m_palmi->removeBouchot(bouchot->name());
+	m_palmi->removeBouchot(bouchot->name());
 
 	QList<QQBouchot *> bouchots = QQBouchot::listBouchotsGroup(group);
 	(bouchots.size() == 0) ?
