3885d3ad74a4410f2b31d5e5dde4299dea6827ee
Damien Guihal <dguihal@gmail.com>
2020-01-27 21:45:38 +0100

    Class cleanup & support for https with eurojirapheau

--- ui/palmipede/qqpalmifileposter.cpp
+++ ui/palmipede/qqpalmifileposter.cpp
@@ -5,20 +5,18 @@
 #include <QFileInfo>
 #include <QHttpMultiPart>
 #include <QStringList>
-#if(QT_VERSION >= QT_VERSION_CHECK(5, 0, 0))
 #include <QJsonArray>
 #include <QJsonDocument>
 #include <QJsonObject>
 #include <QMimeType>
 #include <QMimeDatabase>
-#endif
 
 //////////////////////////////////////////////////////////////
 /// \brief QQPalmiFilePoster::QQPalmiFilePoster
 /// \param parent
 ///
 QQPalmiFilePoster::QQPalmiFilePoster(QObject *parent) :
-	QQNetworkAccessor(parent)
+    QQNetworkAccessor(parent)
 {
 }
 
@@ -37,7 +35,7 @@ bool QQPalmiFilePoster::postFile(const QString &fileName)
 	QFile *file = new QFile(fi.canonicalFilePath());
 	file->open(QIODevice::ReadOnly);
 
-	QNetworkReply * rep = NULL;
+	QNetworkReply * rep = nullptr;
 
 	QQSettings settings;
 	QString sharingService = settings.value(SETTINGS_FILE_SHARING_SERVICE, DEFAULT_FILE_SHARING_SERVICE).toString();
@@ -47,10 +45,10 @@ bool QQPalmiFilePoster::postFile(const QString &fileName)
 	else if (sharingService == FILE_SHARING_SERVICE_JIRAFEAU_EUROMUSSELS_EU)
 		rep = postFileUpload3TerOrg(file);
 
-	if (rep != NULL)
-		connect(rep, SIGNAL(uploadProgress(qint64,qint64)), this, SLOT(uploadProgressSlot(qint64,qint64)));
+	if (rep != nullptr)
+		connect(rep, &QNetworkReply::uploadProgress, this, &QQPalmiFilePoster::uploadProgressSlot);
 
-	return (rep != NULL);
+	return (rep != nullptr);
 }
 
 //////////////////////////////////////////////////////////////
@@ -60,7 +58,7 @@ bool QQPalmiFilePoster::postFile(const QString &fileName)
 void QQPalmiFilePoster::requestFinishedSlot(QNetworkReply *reply)
 {
 	if(reply->error() == QNetworkReply::NoError &&
-			reply->attribute(QNetworkRequest::HttpStatusCodeAttribute).toInt() == 200)
+	        reply->attribute(QNetworkRequest::HttpStatusCodeAttribute).toInt() == 200)
 	{
 		QQSettings settings;
 		QString sharingService = settings.value(SETTINGS_FILE_SHARING_SERVICE, DEFAULT_FILE_SHARING_SERVICE).toString();
@@ -100,15 +98,11 @@ QNetworkReply * QQPalmiFilePoster::postFileUpload3TerOrg(QFile *file)
 	QHttpMultiPart *multiPart = new QHttpMultiPart(QHttpMultiPart::FormDataType);
 
 	QHttpPart filePart;
-#if(QT_VERSION >= QT_VERSION_CHECK(5, 0, 0))
 	QMimeDatabase mimeDatabase;
 	QMimeType mimeType = mimeDatabase.mimeTypeForFile(file->fileName(), QMimeDatabase::MatchDefault);
 	filePart.setHeader(QNetworkRequest::ContentTypeHeader, QVariant(mimeType.name()));
-#else
-	filePart.setHeader(QNetworkRequest::ContentTypeHeader, QVariant("application/octet-stream"));
-#endif
 	filePart.setHeader(QNetworkRequest::ContentDispositionHeader,
-					   QString("form-data; name=\"file\"; filename=\"%1\"").arg(file->fileName()));
+	                   QString("form-data; name=\"file\"; filename=\"%1\"").arg(file->fileName()));
 	filePart.setBodyDevice(file);
 	file->setParent(multiPart); // we cannot delete the file now, so delete it with the multiPart
 
@@ -116,18 +110,13 @@ QNetworkReply * QQPalmiFilePoster::postFileUpload3TerOrg(QFile *file)
 
 	QHttpPart timePart;
 	timePart.setHeader(QNetworkRequest::ContentDispositionHeader,
-					   QString("form-data; name=\"time\""));
+	                   QString("form-data; name=\"time\""));
 	timePart.setBody(QString::fromLatin1("week").toLatin1());
 	multiPart->append(timePart);
 
-	QUrl url(QString("http://%1/script.php").arg(FILE_SHARING_SERVICE_JIRAFEAU_EUROMUSSELS_EU));
+	QUrl url(QString("https://%1/script.php").arg(FILE_SHARING_SERVICE_JIRAFEAU_EUROMUSSELS_EU));
 	QNetworkRequest request(url);
-	//request.setRawHeader("User-Agent", "Mozilla/5.0 (quteqoin)"); // Le service n'accepte pas l'ua par défaut ...
-	//request.setRawHeader("Accept", "application/json, text/javascript, */*; q=0.01");
-	//request.setRawHeader("Accept-Language", "en-US,en;q=0.5");
-	//request.setRawHeader("Accept-Encoding", "gzip, deflate"); // Qt 4 ne supporte pas gunzip nativement ....
-	//request.setRawHeader("X-Requested-With", "XMLHttpRequest");
-	request.setRawHeader("Referer", QString("http://%1/").arg(FILE_SHARING_SERVICE_JIRAFEAU_EUROMUSSELS_EU).toLatin1());
+	request.setRawHeader("Referer", QString("https://%1/").arg(FILE_SHARING_SERVICE_JIRAFEAU_EUROMUSSELS_EU).toLatin1());
 
 	QNetworkReply *reply = httpPost(request, multiPart);
 	multiPart->setParent(reply); // delete the multiPart with the reply
@@ -143,7 +132,7 @@ void QQPalmiFilePoster::parseUpload3TerOrg(const QString &data)
 {
 	QStringList strL = data.split(QChar('\n'));
 	if(strL.size() > 0)
-		emit finished(QString("http://%1/f.php?h=%2&p=1").arg(FILE_SHARING_SERVICE_JIRAFEAU_EUROMUSSELS_EU).arg(strL.first()));
+		emit finished(QString("https://%1/f.php?h=%2&p=1").arg(FILE_SHARING_SERVICE_JIRAFEAU_EUROMUSSELS_EU).arg(strL.first()));
 }
 
 ///////////
