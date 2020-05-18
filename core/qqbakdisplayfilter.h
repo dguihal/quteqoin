e5cce095137bae2e1c685c14148b50274b5d37cb
Damien Guihal <dguihal@gmail.com>
2020-05-18 23:59:42 +0200

    Modernize code

--- core/qqbakdisplayfilter.h
+++ core/qqbakdisplayfilter.h
@@ -9,7 +9,7 @@ public:
 	QQBakDisplayFilter() : QQPostDisplayFilter() {}
 	virtual ~QQBakDisplayFilter() {}
 
-	virtual bool filterMatch(const QQPost *post);
+	virtual bool filterMatch(const QQPost *post) override;
 };
 
 #endif // QQBAKDISPLAYFILTER_H
